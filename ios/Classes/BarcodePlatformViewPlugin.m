b0850d881f2da45a4e89c766679c82eaf5629775
rodrigowerberich <rodrigowerberich@hotmail.com>
2019-04-29 14:54:08 -0300

    Corrected incorrect import

--- ios/Classes/BarcodePlatformViewPlugin.m
+++ ios/Classes/BarcodePlatformViewPlugin.m
@@ -1,5 +1,4 @@
 #import "BarcodePlatformViewPlugin.h"
-#import <barcode_platform_view/barcode_platform_view-Swift.h>
 
 @implementation BarcodePlatformViewPlugin
 // + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
