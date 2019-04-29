b18043cd5a93757b386fdce0f4f169b5f018139c
rodrigowerberich <rodrigowerberich@hotmail.com>
2019-04-29 15:45:36 -0300

    Correcting variable name

--- ios/Classes/BarcodePlatformViewPlugin.m
+++ ios/Classes/BarcodePlatformViewPlugin.m
@@ -6,8 +6,8 @@
 //   [SwiftBarcodePlatformViewPlugin registerWithRegistrar:registrar];
 // }
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterNativeBarcodeScanFactory* barcodeScanFactory =
-      [[FlutterNativeBarcodeScanFactory alloc] initWithMessenger:registrar.messenger];
+  FlutterBarcodeScanFactory* barcodeScanFactory =
+      [[FlutterBarcodeScanFactory alloc] initWithMessenger:registrar.messenger];
   [registrar registerViewFactory:barcodeScanFactory withId:@"barcodescanner"];
 }
 @end
